99a6ee8cc7e5960e2112d915f71fb519a2cfee32
s-jad <s_jad@web.de>
2024-05-15 15:07:47 +0200

    Added: link vec, pending_link to TextArea, basic link recognition logic to insert_char()

---
+++ src/links.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, Clone, Eq, Copy)]
+pub(crate) struct LinkPos {
+    pub(crate) row: usize,
+    pub(crate) col: usize,
+}
+
+impl PartialEq for LinkPos {
+    fn eq(&self, other: &Self) -> bool {
+        self.row == other.row && self.col == other.col
+    }
+}
+
+impl PartialOrd for LinkPos {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.row.partial_cmp(&other.row) {
+            Some(core::cmp::Ordering::Equal) => self.col.partial_cmp(&other.col),
+            ord => return ord,
+        }
+    }
+}
+
+impl Ord for LinkPos {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap_or(std::cmp::Ordering::Equal)
+    }
+}
+
+impl LinkPos {
+    pub(crate) fn new((row, col): (usize, usize)) -> Self {
+        Self { row, col }
+    }
+
+    pub(crate) fn order_positions(self, other: LinkPos) -> (LinkPos, LinkPos) {
+        (std::cmp::min(self, other), std::cmp::max(self, other))
+    }
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct Link {
+    pub(crate) start: LinkPos,
+    pub(crate) end: LinkPos,
+}
+
+impl Link {
+    pub(crate) fn new(start: LinkPos, end: LinkPos) -> Self {
+        Self { start, end }
+    }
+}
