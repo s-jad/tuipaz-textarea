d250b6a0bcebe47f49499d2d17725bb7e1aa98da
s-jad <s_jad@web.de>
2024-05-16 15:36:10 +0200

    Use pre-existing row, links no longer possible over 2 rows

--- src/links.rs
+++ src/links.rs
@@ -1,49 +1,18 @@
-#[derive(Debug, Clone, Eq, Copy)]
-pub struct LinkPos {
-    pub row: usize,
-    pub col: usize,
-}
-
-impl PartialEq for LinkPos {
-    fn eq(&self, other: &Self) -> bool {
-        self.row == other.row && self.col == other.col
-    }
-}
-
-impl PartialOrd for LinkPos {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        match self.row.partial_cmp(&other.row) {
-            Some(core::cmp::Ordering::Equal) => self.col.partial_cmp(&other.col),
-            ord => return ord,
-        }
-    }
-}
-
-impl Ord for LinkPos {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.partial_cmp(other).unwrap_or(std::cmp::Ordering::Equal)
-    }
-}
-
-impl LinkPos {
-    pub(crate) fn new((row, col): (usize, usize)) -> Self {
-        Self { row, col }
-    }
-
-    pub(crate) fn order_positions(self, other: LinkPos) -> (LinkPos, LinkPos) {
-        (std::cmp::min(self, other), std::cmp::max(self, other))
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct Link {
     pub id: usize,
-    pub start: LinkPos,
-    pub end: LinkPos,
+    pub row: usize,
+    pub start_col: usize,
+    pub end_col: usize,
 }
 
 impl Link {
-    pub(crate) fn new(id: usize, start: LinkPos, end: LinkPos) -> Self {
-        Self { id, start, end }
+    pub(crate) fn new(id: usize, row: usize, start_col: usize, end_col: usize) -> Self {
+        Self {
+            id,
+            row,
+            start_col,
+            end_col,
+        }
     }
 }
